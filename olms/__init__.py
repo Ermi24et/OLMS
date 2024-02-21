00c23487bdb8ed4274c896b2d2827c9770fb7624
Ermi24et <lilermi24@gmail.com>
2024-02-21 16:39:53 +0300

    initialization

--- olms/__init__.py
+++ olms/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-# initializing codes and configure the flask application package
+# initializing codes and configure the flask application packages
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///olms.db"
